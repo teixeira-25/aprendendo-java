8168fafe4b65bf3c4f716d761f801a8812694409
Gabriel <gabrielcavalcanteteixeira@gmail.com>
2024-09-09 19:19:40 -0300

    aprendendo metodos especiais

--- Caneta.java
+++ Caneta.java
@@ -1,19 +1,32 @@
 public class Caneta
 {
-    String cor;
-    String marca;
-    boolean tampa = true;
-    public void escrever(String b)
+    private String cor;
+    public String getCor()
     {
-        if (tampa==false)
-        {
-            System.out.println(b);
-        }else
-        {
-            estadoDaTampa();
-            System.out.println("Incapaz de escrever");
-        }
+        return cor;
     }
+    public void setCor(String c)
+    {
+        this.cor = c;
+    }
+    private String marca;
+    public String getMarca()
+    {
+        return marca;
+    }
+    public void setMarca(String marca)
+    {
+        this.marca = marca;
+    }
+
+    public boolean tampa = true;
+    public boolean isTampa() {
+        return tampa;
+    }
+    public void setTampa(boolean tampa) {
+        this.tampa = tampa;
+    }
+
     public void estadoDaTampa()
     {
         if (tampa==true)
@@ -22,6 +35,17 @@ public class Caneta
         } else
         {
             System.out.println("Caneta destampada");
+        }
+    }
+    public void escrever(String b)
+    {
+        if (!tampa)
+        {
+            System.out.println(b);
+        }else
+        {
+            estadoDaTampa();
+            System.out.println("Incapaz de escrever");
         }
     }
     public void estadoDaTampa(short a)
@@ -33,5 +57,23 @@ public class Caneta
         {
             tampa = true;
         }
+    }
+    public Caneta(boolean tampa, String cor, String marca)
+    {
+        this.setTampa(tampa);
+        this.setCor(cor);
+        this.setMarca(marca);
+    }
+    public Caneta()
+    {
+        this.setTampa(true);
+        this.setCor("Azul");
+        this.setMarca("BIC");
+    }
+    public void status()
+    {
+        System.out.println("Cor " + cor );
+        System.out.println("Tampada " + tampa);
+        System.out.println("Marca " + marca);
     }
 }
--- Teste.java
+++ Teste.java
@@ -4,17 +4,19 @@ public class Teste
 {
     public static void main(String[] args)
     {
+        String c = " ";
         Scanner leia = new Scanner(System.in);
-        Caneta tutu;
-        tutu = new Caneta();
-        tutu.cor = "azul";
-        tutu.marca = "bic";
-        System.out.println("A marca da caneta é " + tutu.marca );
-        System.out.println("A cor da caneta é " + tutu.cor);
-        tutu.estadoDaTampa();
+        Caneta tutu = new Caneta();
+        tutu.status();
         System.out.println("Digite [1] para tampar ou [2] para destampar");
         short h = leia.nextShort();
+        leia.nextLine();
         tutu.estadoDaTampa(h);
-        tutu.escrever("Tutu 5° lugar");
+        if (!tutu.tampa)
+        {
+            System.out.println("O que quer que seja escrito?");
+            c =  leia.nextLine();
+        }
+        tutu.escrever(c);
     }
 }
